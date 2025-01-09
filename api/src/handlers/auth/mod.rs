c83de2f4cf9f44efb622f378106027bbfdc4ab1f
Tommie Nygren <tommie.nygren@gmail.com>
2025-01-09 20:47:19 +0100

    WIP OpenID Connect

--- api/src/handlers/auth/mod.rs
+++ api/src/handlers/auth/mod.rs
@@ -1,78 +1,129 @@
-use openidconnect::http::StatusCode;
-use poem::{get, handler, Error, IntoResponse, Route};
-use poem::error::InternalServerError;
-use poem::web::{Data, Form, Html, Path, Query, Redirect};
-use sea_orm::prelude::Uuid;
-use entities::member;
-use crate::{AppState, PaginationParams, DEFAULT_ITEMS_PER_PAGE};
+use openidconnect::core::{
+    CoreAuthDisplay, CoreClaimName, CoreClaimType, CoreClient, CoreClientAuthMethod, CoreGrantType,
+    CoreIdTokenClaims, CoreIdTokenVerifier, CoreJsonWebKey, CoreJweContentEncryptionAlgorithm,
+    CoreJweKeyManagementAlgorithm, CoreResponseMode, CoreResponseType, CoreRevocableToken,
+    CoreSubjectIdentifierType,
+};
+use openidconnect::{reqwest, Client};
+use openidconnect::{
+    AdditionalProviderMetadata, AuthenticationFlow, AuthorizationCode, ClientId, ClientSecret,
+    CsrfToken, IssuerUrl, Nonce, OAuth2TokenResponse, ProviderMetadata, RedirectUrl, RevocationUrl,
+    Scope,
+};
+use serde::{Deserialize, Serialize};
+use url::Url;
 
-#[handler]
-pub async fn login(
-    state: Data<&AppState>,
-    Query(params): Query<PaginationParams>,
-) -> poem::Result<impl IntoResponse> {
-    Ok(Html)
-    Redirect::moved_permanent("https:// www. google. com")
+use anyhow::Result;
+use reqwest::ClientBuilder;
+use std::env;
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpListener;
+use std::process::exit;
+
+// Teach openidconnect-rs about a Google custom extension to the OpenID Discovery response that we can use as the RFC
+// 7009 OAuth 2.0 Token Revocation endpoint. For more information about the Google specific Discovery response see the
+// Google OpenID Connect service documentation at: https://developers.google.com/identity/protocols/oauth2/openid-connect#discovery
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct RevocationEndpointProviderMetadata {
+    revocation_endpoint: String,
 }
+impl AdditionalProviderMetadata for RevocationEndpointProviderMetadata {}
+type GoogleProviderMetadata = ProviderMetadata<
+    RevocationEndpointProviderMetadata,
+    CoreAuthDisplay,
+    CoreClientAuthMethod,
+    CoreClaimName,
+    CoreClaimType,
+    CoreGrantType,
+    CoreJweContentEncryptionAlgorithm,
+    CoreJweKeyManagementAlgorithm,
+    CoreJsonWebKey,
+    CoreResponseMode,
+    CoreResponseType,
+    CoreSubjectIdentifierType,
+>;
 
-#[handler]
-pub async fn new(state: Data<&AppState>) -> poem::Result<impl IntoResponse> {
-    let ctx = tera::Context::new();
-    match state.templates.render("members/new.html.tera", &ctx) {
-        Ok(rendered) => Ok(Html(rendered)),
-        Err(err) => {
-            eprintln!("Tera rendering error: {:?}", err); // Log the error
-            Err(InternalServerError(err))
-        }
+fn handle_error<T: std::error::Error>(fail: &T, msg: &'static str) {
+    let mut err_msg = format!("ERROR: {}", msg);
+    let mut cur_fail: Option<&dyn std::error::Error> = Some(fail);
+    while let Some(cause) = cur_fail {
+        err_msg += &format!("\n    caused by: {}", cause);
+        cur_fail = cause.source();
     }
+    println!("{}", err_msg);
+    exit(1);
 }
 
-#[handler]
-pub async fn edit(state: Data<&AppState>, Path(id): Path<Uuid>) -> poem::Result<impl IntoResponse> {
-    let conn = &state.conn;
+async fn setup_openid_client() -> Result<CoreClient> {
+    let google_client_id = ClientId::new(
+        env::var("GOOGLE_CLIENT_ID").expect("Missing the GOOGLE_CLIENT_ID environment variable."),
+    );
+    let google_client_secret = ClientSecret::new(
+        env::var("GOOGLE_CLIENT_SECRET")
+            .expect("Missing the GOOGLE_CLIENT_SECRET environment variable."),
+    );
+    let issuer_url =
+        IssuerUrl::new("https://accounts.google.com".to_string()).unwrap_or_else(|err| {
+            handle_error(&err, "Invalid issuer URL");
+            unreachable!();
+        });
 
-    let member: member::Model = QueryCore::find_member_by_id(conn, id)
+    let http_client = ClientBuilder::new()
+        // Following redirects opens the client up to SSRF vulnerabilities.
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .unwrap_or_else(|err| {
+            handle_error(&err, "Failed to build HTTP client");
+            unreachable!();
+        });
+
+    // Fetch Google's OpenID Connect discovery document.
+    //
+    // Note: If we don't care about token revocation we can simply use CoreProviderMetadata here
+    // instead of GoogleProviderMetadata. If instead we wanted to optionally use the token
+    // revocation endpoint if it seems to be supported we could do something like this:
+    //   #[derive(Clone, Debug, Deserialize, Serialize)]
+    //   struct AllOtherProviderMetadata(HashMap<String, serde_json::Value>);
+    //   impl AdditionalClaims for AllOtherProviderMetadata {}
+    // And then test for the presence of "revocation_endpoint" in the map returned by a call to
+    // .additional_metadata().
+
+    let provider_metadata = GoogleProviderMetadata::discover_async(issuer_url, &http_client)
         .await
-        .map_err(InternalServerError)?
-        .ok_or_else(|| Error::from_status(StatusCode::NOT_FOUND))?;
+        .unwrap_or_else(|err| {
+            handle_error(&err, "Failed to discover OpenID Provider");
+            unreachable!();
+        });
 
-    let mut ctx = tera::Context::new();
-    ctx.insert("member", &member);
+    let revocation_endpoint = provider_metadata
+        .additional_metadata()
+        .revocation_endpoint
+        .clone();
+    println!(
+        "Discovered Google revocation endpoint: {}",
+        revocation_endpoint
+    );
 
-    let body = state
-        .templates
-        .render("members/edit.html.tera", &ctx)
-        .map_err(InternalServerError)?;
-    Ok(Html(body))
-}
-
-#[handler]
-pub async fn update(
-    state: Data<&AppState>,
-    Path(id): Path<Uuid>,
-    form: Form<member::Model>,
-) -> poem::Result<impl IntoResponse> {
-    let conn = &state.conn;
-    let form = form.0;
-
-    let member = MutationCore::update_member_by_id(conn, id, form)
-        .await
-        .map_err(InternalServerError)?;
-
-    let mut ctx = tera::Context::new();
-    ctx.insert("member", &member);
-
-    let body = state
-        .templates
-        .render("members/member_row.html.tera", &ctx)
-        .map_err(InternalServerError)?;
-    Ok(Html(body))
-}
-
-// A function to define all routes related to posts
-pub fn routes() -> Route {
-    Route::new()
-        .at("/", get(list).post(create))
-        .at("/new", get(new))
-        .at("/:id", get(edit).patch(update).delete(destroy))
+    // Set up the config for the Google OAuth2 process.
+    let client = CoreClient::from_provider_metadata(
+        provider_metadata,
+        google_client_id,
+        Some(google_client_secret),
+    )
+    // This example will be running its own server at localhost:8080.
+    // See below for the server implementation.
+    .set_redirect_uri(
+        RedirectUrl::new("http://localhost:8080".to_string()).unwrap_or_else(|err| {
+            handle_error(&err, "Invalid redirect URL");
+            unreachable!();
+        }),
+    )
+    // Google supports OAuth 2.0 Token Revocation (RFC-7009)
+    .set_revocation_url(
+        RevocationUrl::new(revocation_endpoint).unwrap_or_else(|err| {
+            handle_error(&err, "Invalid revocation endpoint URL");
+            unreachable!();
+        }),
+    );
+    Ok(client)
 }
