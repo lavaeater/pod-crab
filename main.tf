1ae094de95b110595b50a2c41e679b2fa99035c0
Tommie Nygren <tommie.nygren@gmail.com>
2025-01-13 19:33:01 +0100

    Adds some stuff for uploading episodes to S3.
    Goal is to do it with a presigned url

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket  = "bealo-terraform-state"
+    key     = "terraform.tfstate"
+    region  = "eu-west-1"
+    profile = "bealo"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.83.1"
     }
   }
 
@@ -14,11 +20,16 @@ provider "aws" {
   profile = "bealo"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-01f5f2e96f603b15b"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+resource "aws_s3_bucket" "episode-storage" {
+  bucket        = "tommie-the-brain-in-space-media"
+  force_destroy = true
 }
+
+# resource "aws_instance" "pod-crab-server" {
+#   ami           = "ami-01f5f2e96f603b15b"
+#   instance_type = "t2.micro"
+# 
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
