7a31c32d8b6247eb62551eb1fec8cfec6dfaccdb
Tommie Nygren <tommie.nygren@gmail.com>
2025-04-05 09:36:14 +0200

    Adds import_row and relations to import.

--- entities/src/import.rs
+++ entities/src/import.rs
@@ -14,6 +14,15 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(has_many = "super::import_row::Entity")]
+    ImportRow,
+}
+
+impl Related<super::import_row::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::ImportRow.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
