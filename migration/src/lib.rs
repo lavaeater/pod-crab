d28704498bf563e2160df07be79c2e5270740953
Tommie Nygren <tommie.nygren@gmail.com>
2025-04-11 14:50:38 +0200

    Commented out the new migration for member events.

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -20,7 +20,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250108_130829_add_episode_and_user_table::Migration),
             Box::new(m20250405_063228_create_import::Migration),
             Box::new(m20250410_123829_create_transactions::Migration),
-            Box::new(m20250410_195329_create_member_events::Migration),
+            // Box::new(m20250410_195329_create_member_events::Migration),
         ]
     }
 }
